65237bf2a3927211407cc3ad1ce3e75e16b583be
hpes04214 <hpes04214@gmail.com>
2023-05-17 20:49:29 +0800

    sign up

---
+++ Web411030787/Controllers/SignUpController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.Ajax.Utilities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Web411030787.Controllers
+{
+    public class SignUpController : Controller
+    {
+        // GET: SignUp
+        public ActionResult SignUp()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult SignUp(string name, string account, string password)
+        {
+            if (name.IsNullOrWhiteSpace())
+            {
+                ViewBag.NameMessage = "請輸入姓名";
+            }
+            if (name.IsNullOrWhiteSpace())
+            {
+                ViewBag.AccountMessage = "請輸入帳號";
+            }
+            if (name.IsNullOrWhiteSpace())
+            {
+                ViewBag.PasswordMessage = "請輸入密碼";
+            }
+
+
+
+
+
+            return View();
+        }
+    }
+}
